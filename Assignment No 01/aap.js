ad84ee610860b05598dcc01be5b471d785b9ee19
Misbah1Khan <121508837+Misbah1Khan@users.noreply.github.com>
2023-10-20 20:44:51 +0500

    Add files via upload

---
+++ Assignment No 01/aap.js
@@ -0,0 +1,126 @@
+//CHAPTER NO: 01
+
+//Question No: 01
+
+alert("Hello");
+
+//Question No: 02
+
+alert("Error! Please enter a valid password.");
+
+//Question No: 03
+
+alert("Welcome to JS Land...\nHappy Coding");
+
+//Question No: 04
+
+alert("Welcome to JS Land...");
+alert("Happy Coding");
+
+//Question No: 05
+
+alert("Hello...I can run JS through my web browser's console");
+
+//CHAPTER NO: 02
+
+//Question No: 01
+
+var UserName = "Misbah Khan"
+alert(UserName)
+
+//Question No: 02
+
+var MyName = "Misbah Ur Rehman Khan"
+alert(MyName)
+
+//Question No: 03
+
+var message = "Hello World"
+alert(message)
+
+//Question No: 04
+
+var Name = "Misbah Ur Rehman Khan"
+var Age = 19
+var ocp = "Certified Web & Mobile App Developer"
+alert(Name)
+alert(Age+" years old")
+alert(ocp)
+
+//Question No: 05
+
+var pizz = "Pizza\npizz\npiz\npi\np"
+alert(pizz)
+
+
+var email = "misbahkhan8036@gmail.com"
+alert("My email address is "+ email)
+
+//Question No: 07
+
+var book = "A smarter way to learn JavaScript"
+alert("I am trying to learn from " + book)
+
+//Question No: 08
+
+document.write("Yah! I can write HTML content through JAVASCRIPT <br/>");
+
+//Question No: 07
+
+document.write("<br/>▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬<br/>")
+var logo = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬" 
+alert(logo)
+
+//CHAPTER NO: 03
+
+//Question No: 01
+
+var Age = 19
+alert(Age+" years old")
+
+//Question No: 02
+
+var visit = 10
+alert("You have visited this site " + visit +" times")
+
+//Question No: 03
+
+var dob = 2004
+document.write("<br/>My date of birth is "+ dob +"<br/>")
+
+//Question No: 04
+
+var nam =prompt("Enter your Name")
+var product = prompt("Enter your Product")
+var quan = prompt("Enter quantity of the product")
+document.write("<br/>"+nam+ " ordered "+ quan+" "+ product+ " form MK clothing store<br/>")
+
+//CHAPTER NO: 04
+
+//Question No: 01
+
+var last ,first, mid 
+
+//Question No: 02
+
+//Illegal Variable
+/*var First-Name
+var 123name
+var first name
+var for
+var @first
+
+//legal Variable
+var last_name
+var name123
+var $name
+var _name
+*/
+//Question No: 03
+
+document.write("<br/><h2>Rules for naming JS variables</h2>")
+document.write("<br />a) Variable names can only contain _____ , _____ , _____ and _____.For example <b>$my_1stVariable</b>.<br />")
+document.write("<br />b) Variables must begin with a ______, ______ or _____.For example <b>$name, _name</b> or <b>name</b>.<br />");
+document.write("<br />c) Variable names are case _________.<br />")
+document.write("<br />d) Variable names should not be JS _________.")
+
